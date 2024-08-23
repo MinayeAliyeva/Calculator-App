c50fd53d3ab33199ffb6a4e63b72f37303e083f9
MinayeAliyeva <7s346pu@code.edu.az>
2024-08-23 12:57:00 +0400

    refactor(refactor/separate-folder-helper):helpers added

--- my-app/src/components/Calculator.tsx
+++ my-app/src/components/Calculator.tsx
@@ -1,94 +1,12 @@
 import { useCallback, useState } from "react";
 import CalcBtn from "./Button";
 import { Box } from "@mui/material";
-
+import {
+  applyOperator,
+  evaluateExpression,
+} from "../helpers/evaluateExpression";
 const Calculator = () => {
   const [display, setDisplay] = useState("");
-
-  const applyOperator = (values: number[], operator: string) => {
-    const b = values.pop();
-    const a = values.pop();
-    if (a === undefined || b === undefined) {
-      throw new Error("Invalid expression");
-    }
-    switch (operator) {
-      case "+":
-        values.push(a + b);
-        break;
-      case "-":
-        values.push(a - b);
-        break;
-      case "*":
-        values.push(a * b);
-        break;
-      case "/":
-        values.push(a / b);
-        break;
-      default:
-        throw new Error(`Unknown operator: ${operator}`);
-    }
-  };
-
-  const evaluateExpression = (expression: string) => {
-    const operators: string[] = [];
-    const values: number[] = [];
-    let num = "";
-
-    const getPrecedence = (op: string) => {
-      interface IOperators {
-        [key: string]: number;
-      }
-      const obj: IOperators = {
-        "+": 1,
-        "-": 1,
-        "*": 2,
-        "/": 2,
-      };
-
-      return obj?.[op];
-    };
-
-    const applyOperatorWithPrecedence = (minPrecedence: number) => {
-      while (
-        operators.length &&
-        getPrecedence(operators[operators.length - 1]) >= minPrecedence
-      ) {
-        applyOperator(values, operators.pop()!);
-      }
-    };
-
-    for (const char of expression) {
-      if ("0123456789.".includes(char)) {
-        num += char;
-      } else {
-        if (num) {
-          values.push(parseFloat(num));
-          num = "";
-        }
-        if (char === "(") {
-          operators.push(char);
-        } else if (char === ")") {
-          while (operators.length && operators[operators.length - 1] !== "(") {
-            applyOperatorWithPrecedence(1);
-            applyOperator(values, operators.pop()!);
-          }
-          operators.pop(); // Remove '('
-        } else if ("*/".includes(char)) {
-          applyOperatorWithPrecedence(2);
-          operators.push(char);
-        } else if ("+-".includes(char)) {
-          applyOperatorWithPrecedence(1);
-          operators.push(char);
-        }
-      }
-    }
-    if (num) {
-      values.push(parseFloat(num));
-    }
-    applyOperatorWithPrecedence(1);
-    return values.pop()!;
-  };
-
   const handleEqual = useCallback(() => {
     try {
       const returnStrFromEval = String(evaluateExpression(display));
