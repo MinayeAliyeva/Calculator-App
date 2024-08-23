8abc9d91ee90d73d17a31419f6a2516e2a79926b
MinayeAliyeva <7s346pu@code.edu.az>
2024-08-24 00:12:24 +0400

    feauture(feauture/calculator--dark-light-mood):Add dark/light mode feature to calculator

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 import Calculator from "./components/Calculator";
 
 function App() {
-  return <div className="App">
-    <Calculator/>
-  </div>;
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
