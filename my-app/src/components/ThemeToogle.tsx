7bcc937eb0be0a504e727b261b6d99fa03f4d9b9
MinayeAliyeva <7s346pu@code.edu.az>
2024-08-27 23:47:39 +0400

    bugfix(bugfix/evaluateExpression-actions-and-calculator-display):Update evaluateExpression function to handle square root and exponentiation with correct precedence

--- my-app/src/components/ThemeToogle.tsx
+++ my-app/src/components/ThemeToogle.tsx
@@ -1,15 +1,13 @@
-import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 import { useTheme } from "../context/ThemeContext";
-
 
 const ThemeToggle = () => {
   const { isDarkMode, toggleTheme } = useTheme();
 
   return (
-    <MdOutlineDarkMode
+    <div
       style={{
-        marginTop: "1rem",
-    
+        marginTop: "0rem",
         color: isDarkMode ? "#fff" : "#000",
         border: "none",
         padding: "8px 16px",
@@ -17,9 +15,17 @@ const ThemeToggle = () => {
         cursor: "pointer",
         transition: "background-color 0.3s ease, color 0.3s ease",
         margin: "20px",
+        display: "flex",
+        alignItems: "center",
       }}
       onClick={toggleTheme}
-    />
+    >
+      {isDarkMode ? (
+        <MdOutlineLightMode size={24} />
+      ) : (
+        <MdOutlineDarkMode size={24} />
+      )}
+    </div>
   );
 };
 
