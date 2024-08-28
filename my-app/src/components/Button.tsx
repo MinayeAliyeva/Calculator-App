4d6a80668ab154bffbf58989c86bcbedd0de4441
MinayeAliyeva <7s346pu@code.edu.az>
2024-08-28 10:35:57 +0400

    style(feature/refactor-modularize-code):button color changings

--- my-app/src/components/Button.tsx
+++ my-app/src/components/Button.tsx
@@ -1,35 +1,36 @@
-import React from 'react';
-import { Button } from '@mui/material';
-import { ICalckBtn } from '../types/interface';
+import React from "react";
+import { Button } from "@mui/material";
+import { ICalckBtn } from "../types/interface";
 
-const CalcBtn = ({ content, onClick }:ICalckBtn) => {
+const CalcBtn = ({ content, onClick }: ICalckBtn) => {
   const operators = ["+", "-", "*", "/", "="];
   const isOperator = operators.includes(content);
-  const isSpecial = content === 'C' || content === 'AC';
+  const isSpecial = content === "C" || content === "AC";
 
   return (
     <Button
       onClick={onClick}
       sx={{
-        width: '92%', 
-        height: '72px',
-        margin: '8px 5px', 
-        borderRadius: '50%',
-        fontSize: '28px',
+        width: "92%",
+        height: "72px",
+        margin: "8px 5px",
+        borderRadius: "50%",
+        fontSize: "28px",
         backgroundColor: isOperator
-          ? '#FF9500' 
+          ? "#FF9500"
           : isSpecial
-          ? '#D4D4D2'
-          : '#333333',
-        color: '#fff', 
-        '&:hover': {
+          ? "#D4D4D2"
+          : "#333333",
+
+        color: isSpecial ? "black" : "white",
+        "&:hover": {
           backgroundColor: isOperator
-            ? '#FF9500'
+            ? "#FF9500"
             : isSpecial
-            ? '#D4D4D2'
-            : '#505050', 
+            ? "#D4D4D2"
+            : "#505050",
         },
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)', 
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
       }}
     >
       {content}
